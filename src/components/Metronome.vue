8ad66270e5ea32562d0dec0f485b16989ab9eeb1
Louis Headley <louish10@hotmail.co.uk>
2023-08-01 10:25:15 +0100

    Create initial scaffolding

---
+++ src/components/Metronome.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import TempoSelect from './TempoSelect.vue'
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+const running = ref(false)
+const tempo = ref(60)
+
+function toggleRunning() {
+  running.value = !running.value
+}
+
+function changeTempo(newTempo) {
+  tempo.value = newTempo
+}
+</script>
+
+<template>
+  <div>
+    <h1>Metrognome {{ tempo }}</h1>
+  </div>
+  <div>{{ running }}</div>
+  <button @click="toggleRunning">{{ running ? 'stop' : 'start' }}</button>
+  <TempoSelect @tempo-change="changeTempo"></TempoSelect>
+</template>
+
+<style scoped></style>
---
+++ src/components/TempoSelect.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const tempo = ref(60)
+</script>
+
+<template>
+  <input v-on:change="$emit('tempoChange', 60)" type="radio" id="60" value="60" v-model="tempo" />
+  <label for="60">60</label>
+  <input v-on:change="$emit('tempoChange', 80)" type="radio" id="80" value="80" v-model="tempo" />
+  <label for="80">80</label>
+</template>
