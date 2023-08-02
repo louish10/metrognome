95286b4b84fbdc3e28d27f582c8170c508f695ae
Louis Headley <louish10@hotmail.co.uk>
2023-08-02 22:45:40 +0100

    Change range accent colour

--- src/components/TempoSelect.vue
+++ src/components/TempoSelect.vue
@@ -7,6 +7,7 @@ defineEmits(['tempoChange'])
 <template>
   <label class="tempo-value">Tempo: {{ tempo }} bpm</label>
   <input
+    class="slider"
     v-model="tempo"
     v-on:change="$emit('tempoChange', tempo)"
     type="range"
@@ -18,5 +19,9 @@ defineEmits(['tempoChange'])
 <style scoped>
 .tempo-value {
   font-size: 24px;
+}
+
+.slider {
+  accent-color: #39ff14;
 }
 </style>
