73a45758e03211899276576345f998e4ca92ad65
Louis Headley <louish10@hotmail.co.uk>
2023-08-09 22:10:35 +0100

    Style elements for a more cohesive look

--- src/components/Metronome.vue
+++ src/components/Metronome.vue
@@ -80,7 +80,7 @@ function changeTempo(newTempo) {
       <h1 class="header">Metrognome</h1>
     </div>
     <TempoSelect @tempo-change="changeTempo"></TempoSelect>
-    <button class="start-button" @click="toggleRunning">{{ running ? 'stop' : 'start' }}</button>
+    <button class="start-button" @click="toggleRunning">{{ running ? 'Stop' : 'Start' }}</button>
   </div>
 </template>
 
@@ -100,12 +100,12 @@ function changeTempo(newTempo) {
 .start-button {
   background-color: transparent;
   border: 2px solid #39ff14;
-  border-radius: 10px;
+  border-radius: 16px;
   color: #39ff14;
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
-  font-size: 24px;
+  font-size: 48px;
 }
 </style>
--- src/components/TempoSelect.vue
+++ src/components/TempoSelect.vue
@@ -31,7 +31,7 @@ function incrementTempo() {
 </script>
 
 <template>
-  <label class="tempo-value">Tempo: {{ tempo }} bpm</label>
+  <label class="tempo-value">{{ tempo }} bpm</label>
   <input
     class="slider"
     v-model="tempo"
@@ -44,10 +44,38 @@ function incrementTempo() {
 
 <style scoped>
 .tempo-value {
-  font-size: 24px;
+  font-size: 48px;
+  text-align: center;
 }
 
 .slider {
   accent-color: #39ff14;
+  -webkit-appearance: none;
+  appearance: none;
+  width: max;
+  height: 40px;
+  margin: 20px 0px;
+  background-color: inherit;
+  border: 5px solid #39ff14;
+  border-radius: 15px;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 40px;
+  height: 40px;
+  background-color: #39ff14;
+  border: none;
+  border-radius: 0px;
+}
+
+.slider::-moz-range-thumb {
+  appearance: none;
+  width: 40px;
+  height: 40px;
+  background-color: #39ff14;
+  border: none;
+  border-radius: 0px;
 }
 </style>
